4bd8f9eb4a436bb011dd38cd6c3091eb25442fbc
Jakub Kretkowski <kreciak09+github@gmail.com>
2026-07-01 19:02:23 +0200

    Remove unnecessary definitions
    i stole this from a project, for absolute no reason.

--- source/PredFunc.c
+++ source/PredFunc.c
@@ -9,32 +9,8 @@
 
 // ------------- WINAPI BATTERY FUNCTIONS ----------
 
-bool ACLineStatus(SYSTEM_POWER_STATUS sps){
-	wchar_t* FUNCNAME = L"ACLineStatus";
-	if(GetSystemPowerStatus(&sps)){
-		return sps.ACLineStatus;
-	} else if(GetSystemPowerStatus(&sps) == 255){	// if Battery is unknown
-		wprintf(L"Battery status '%ls' is unknown (returned 255)\n", FUNCNAME);
-	} else {
-		wprintf(L"Failed to get status: '%ls'.\n", FUNCNAME);
-	}
-}
-
 LRESULT SetSystemThemeFont(HWND hwnd){
 	HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
 	return SendMessage(hwnd, WM_SETFONT, (WPARAM)hFont, TRUE);
-}
-
-// --------------- STRING & OTHER FUNCTIONS -------------
-
-wchar_t* FetchStringW(HINSTANCE hInstance, int strID) {
-	wchar_t* FUNCNAME = L"FetchStringW";
-	static wchar_t Buffer[3072];
-	
-	if(LoadStringW(hInstance, strID, Buffer, sizeof(Buffer) / sizeof(wchar_t)) > 0){
-		return Buffer;
-	} else {
-		wprintf(L"%ls failed to Fetch", FUNCNAME);
-	}
 }
 
